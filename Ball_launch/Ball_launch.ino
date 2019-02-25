5ac366f66310cc1bdae60fe9263304a2ec381aca
Amber Liu <amberyueliu@gmail.com>
2019-02-24 23:55:32 -0500

    new error about pointers

--- Ball_launch/Ball_launch.ino
+++ Ball_launch/Ball_launch.ino
@@ -13,6 +13,8 @@ const int DT = 40; //milliseconds
 const int EXCITEMENT = 2000; //how much force to apply to ball
 const uint8_t input_pin1 = 16; // apply force
 const uint8_t input_pin2 = 5; // reset screen
+uint8_t launch = digitalRead(input_pin1);
+uint8_t resetS = digitalRead(input_pin2);
 
 bool pushed_last_time; //for finding change of button (using bool type...same as uint8_t)
 uint8_t push_count;
@@ -78,61 +80,60 @@ uint8_t state_4 = 0;
 
 MPU9255 imu; //imu object called, appropriately, imu
 
-void moveBall(float x_pos, float y_pos, float x_vel, float y_vel){
-  float nextX = x_pos + 0.001*DT*x_vel;
-  float nextY = y_pos + 0.001*DT*y_vel;
+void moveBall(float* x_pos, float* y_pos, float* x_vel, float* y_vel){
+  float nextX = *x_pos + 0.001*DT*(*x_vel);
+  float nextY = *y_pos + 0.001*DT*(*y_vel);
   if (nextX < LEFT_LIMIT) {
-    x_pos = LEFT_LIMIT+ K_SPRING*(LEFT_LIMIT - nextX);
-    x_vel = -K_SPRING*x_vel;
+    *x_pos = LEFT_LIMIT+ K_SPRING*(LEFT_LIMIT - nextX);
+    *x_vel = -K_SPRING*(*x_vel);
   } else if (nextX > RIGHT_LIMIT) {
-    x_pos = RIGHT_LIMIT- K_SPRING*(nextX - RIGHT_LIMIT);
-    x_vel = -K_SPRING*x_vel;
+    *x_pos = RIGHT_LIMIT- K_SPRING*(nextX - RIGHT_LIMIT);
+    *x_vel = -K_SPRING*(*x_vel);
   } else {
-    x_pos = nextX;
+    *x_pos = nextX;
   }
 
   if (nextY > BOTTOM_LIMIT) {
-    y_pos = BOTTOM_LIMIT- K_SPRING*(nextY - BOTTOM_LIMIT);
-    y_vel = -K_SPRING*y_vel;
+    *y_pos = BOTTOM_LIMIT- K_SPRING*(nextY - BOTTOM_LIMIT);
+    *y_vel = -K_SPRING*(*y_vel);
   } else if (nextY < TOP_LIMIT) {
-    y_pos = TOP_LIMIT+K_SPRING*(TOP_LIMIT - nextY);
-    y_vel = -K_SPRING*y_vel;
+    *y_pos = TOP_LIMIT+K_SPRING*(TOP_LIMIT - nextY);
+    *y_vel = -K_SPRING*(*y_vel);
   } else {
-    y_pos = nextY;
+    *y_pos = nextY;
   }
 }
 
-void step(float x_pos, float y_pos, float x_vel, float y_vel, float x_accel, float y_accel, float x_force=0, float y_force=0){
+void step(float* x_pos, float* y_pos, float* x_vel, float* y_vel, float* x_accel, float* y_accel, float x_force=0, float y_force=0){
   //update acceleration (from f=ma)
-  x_accel = (x_force-K_FRICTION*x_vel)/MASS;
-  y_accel = (y_force-K_FRICTION*y_vel)/MASS;
+  *x_accel = (x_force-K_FRICTION*(*x_vel))/MASS;
+  *y_accel = (y_force-K_FRICTION*(*y_vel))/MASS;
   //integrate to get velocity from current acceleration
-  x_vel = x_vel + 0.001*DT*x_accel; //integrate, 0.001 is conversion from milliseconds to seconds
-  y_vel = y_vel + 0.001*DT*y_accel; //integrate
+  *x_vel = *x_vel + 0.001*DT*(*x_accel); //integrate, 0.001 is conversion from milliseconds to seconds
+  *y_vel = *y_vel + 0.001*DT*(*y_accel); //integrate
   //
-  moveBall(x_pos, y_pos, x_vel, y_vel);
+  moveBall(&x_pos, &y_pos, &x_vel, &y_vel);
 }
 
-void ball_update(uint8_t state, uint8_t launch, float x_pos, float y_pos, float x_vel, float y_vel, float x_accel, float y_accel) { // pass in the state of the ball (declared globally)
-  switch (state) {
+void ball_update(uint8_t* state, uint8_t* launch, float* x_pos, float* y_pos, float* x_vel, float* y_vel, float* x_accel, float* y_accel) { // pass in the state of the ball (declared globally)
+  switch (*state) {
     case 0: //initialize ball (stationary)
-      if (!launch) { // if pushed
-        state = 1; //change state
-      } else {
-        tft.fillCircle(x_pos_d,y_pos_d,RADIUS,BALL_COLOR); //draw ball
+      if (!*launch) { // if pushed
+        *state = 1; //change state
       }
+      tft.fillCircle(x_pos_d,y_pos_d,RADIUS,BALL_COLOR); //draw ball
     break;    
     case 1: // update acceleration
-      if (!launch) { // if pushed
+      if (!*launch) { // if pushed
         tft.fillCircle(x_pos_d,y_pos_d,RADIUS,BALL_COLOR);
         // calculate acceleration
         imu.readAccelData(imu.accelCount);//read imu
         y = -imu.accelCount[0]*imu.aRes;
         x = -imu.accelCount[1]*imu.aRes;
         tft.fillCircle(x_pos_d,y_pos_d,RADIUS,BALL_COLOR);
+        *state = 2;
       } else {
-        tft.fillCircle(x_pos_d,y_pos_d,RADIUS,BALL_COLOR);
-        state = 2;
+        tft.fillCircle(x_pos_d,y_pos_d,RADIUS,BALL_COLOR);        
       }
     break;
     case 2: // call step() with updated acceleration once released botton
@@ -140,14 +141,14 @@ void ball_update(uint8_t state, uint8_t launch, float x_pos, float y_pos, float 
       if (push_count < 4){
         push_count++;
       }
-      step(x_pos, y_pos, x_vel, y_vel, x_accel, y_accel, x*EXCITEMENT, y*EXCITEMENT);//apply force based on last acceleration
-      tft.fillCircle(x_pos,y_pos,RADIUS,BALL_COLOR);
-      state = 3;
+      step(&x_pos, &y_pos, &x_vel, &y_vel, &x_accel, &y_accel, x*EXCITEMENT, y*EXCITEMENT);//apply force based on last acceleration
+      tft.fillCircle(*x_pos,*y_pos,RADIUS,BALL_COLOR);
+      *state = 3;
     break;
     case 3: // final state, stop updating accel
-      tft.fillCircle(x_pos,y_pos,RADIUS,BALL_COLOR);
-      step(x_pos, y_pos, x_vel, y_vel, x_accel, y_accel);
-      tft.fillCircle(x_pos,y_pos,RADIUS,BALL_COLOR);
+      tft.fillCircle(*x_pos,*y_pos,RADIUS,BALL_COLOR);
+      step(&x_pos, &y_pos, &x_vel, &y_vel, &x_accel, &y_accel);
+      tft.fillCircle(*x_pos,*y_pos,RADIUS,BALL_COLOR);
     break;
 
     default: // handling unexpected behavior
@@ -156,35 +157,35 @@ void ball_update(uint8_t state, uint8_t launch, float x_pos, float y_pos, float 
   }
 }
 
-void ball_1(uint8_t launch, float x_pos, float y_pos, float x_vel, float y_vel, float x_accel, float y_accel) {
+void ball_1(uint8_t launch, float* x_pos, float* y_pos, float* x_vel, float* y_vel, float* x_accel, float* y_accel) {
   if (push_count == 0) {
-    ball_update(state_1, launch, x_pos, y_pos, x_vel, y_vel, x_accel, y_accel);
+    ball_update(&state_1, launch, &x_pos, &y_pos, &x_vel, &y_vel, &x_accel, &y_accel);
   }
 }
 
-void ball_2(uint8_t launch, float x_pos, float y_pos, float x_vel, float y_vel, float x_accel, float y_accel) {
+void ball_2(uint8_t launch, float* x_pos, float* y_pos, float* x_vel, float* y_vel, float* x_accel, float* y_accel) {
   if (push_count == 1) {
-    ball_update(state_2, launch, x_pos, y_pos, x_vel, y_vel, x_accel, y_accel);
+    ball_update(&state_2, launch, &x_pos, &y_pos, &x_vel, &y_vel, &x_accel, &y_accel);
   }
 }
 
-void ball_3(uint8_t launch, float x_pos, float y_pos, float x_vel, float y_vel, float x_accel, float y_accel) {
+void ball_3(uint8_t launch, float* x_pos, float* y_pos, float* x_vel, float* y_vel, float* x_accel, float* y_accel) {
   if (push_count == 2) {
-    ball_update(state_3, launch, x_pos, y_pos, x_vel, y_vel, x_accel, y_accel);
+    ball_update(&state_3, launch, &x_pos, &y_pos, &x_vel, &y_vel, &x_accel, &y_accel);
   }
 }
 
-void ball_4(uint8_t launch, float x_pos, float y_pos, float x_vel, float y_vel, float x_accel, float y_accel) {
+void ball_4(uint8_t launch, float* x_pos, float* y_pos, float* x_vel, float* y_vel, float* x_accel, float* y_accel) {
   if (push_count == 3) {
-    ball_update(state_4, launch, x_pos, y_pos, x_vel, y_vel, x_accel, y_accel);
+    ball_update(&state_4, launch, &x_pos, &y_pos, &x_vel, &y_vel, &x_accel, &y_accel);
   }
 }
 
 void run_all_balls(uint8_t launch) {
-  ball_1(launch, x_pos_1, y_pos_1, x_vel_1, y_vel_1, x_accel_1, y_accel_1);
-  ball_2(launch, x_pos_2, y_pos_2, x_vel_2, y_vel_2, x_accel_2, y_accel_2);
-  ball_3(launch, x_pos_3, y_pos_3, x_vel_3, y_vel_3, x_accel_3, y_accel_3);
-  ball_4(launch, x_pos_4, y_pos_4, x_vel_4, y_vel_4, x_accel_4, y_accel_4);
+  ball_1(launch, &x_pos_1, &y_pos_1, &x_vel_1, &y_vel_1, &x_accel_1, &y_accel_1);
+  ball_2(launch, &x_pos_2, &y_pos_2, &x_vel_2, &y_vel_2, &x_accel_2, &y_accel_2);
+  ball_3(launch, &x_pos_3, &y_pos_3, &x_vel_3, &y_vel_3, &x_accel_3, &y_accel_3);
+  ball_4(launch, &x_pos_4, &y_pos_4, &x_vel_4, &y_vel_4, &x_accel_4, &y_accel_4);
 }
 
 void setup() {
@@ -208,8 +209,8 @@ void setup() {
 }
  
 void loop() {
-  uint8_t launch = digitalRead(input_pin1);
-  uint8_t resetS = digitalRead(input_pin2);
+  launch = digitalRead(input_pin1);
+  resetS = digitalRead(input_pin2);
 
   /*state machine for resettting
   once pushed, before even release, will reset the program
